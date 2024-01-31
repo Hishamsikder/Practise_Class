16167d2622d28cc802fe98041a5c1897c26e63e3
Sanim018 <you@example.com>
2024-01-31 11:15:13 +0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,142 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
+        appBar: AppBar(
+          title: const Text("Practise App"),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 30,
+              ),
+              Center(
+                child: Container(
+                  height: 50,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.amber,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.shade600,
+                        offset: const Offset(0, 4),
+                        blurRadius: 5,
+                        spreadRadius: 1
+                      )
+                    ]
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Container(
+                color: Colors.amber,
+                child: const Text("Class 3 Class 4"),
+              ),
+              const SizedBox(height: 30,),
+              Container(
+                height: 50,
+                width: 100,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.amber,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.shade600,
+                          offset: const Offset(0, -4),
+                          blurRadius: 5,
+                          spreadRadius: 1
+                      )
+                    ]
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 50,
+                width: 300,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.green),
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        offset: const Offset(4, 4),
+                          color: Colors.grey.shade600,
+                          blurRadius: 5,
+                          spreadRadius: 1
+                      ),
 
+                    ]
+                ),
+                child: const Center(child: Padding(
+                  padding: EdgeInsets.only(right: 170.0),
+                  child: Text("influxdev.com"),
+                )),
+              ),
+              const SizedBox(height: 20,),
+              Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.red,
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text("Red",style: TextStyle(
+                        fontWeight: FontWeight.bold,color: Colors.white
+                      ),),
+                    ),
+                  ),
+                  Positioned(
+                      top: 30,
+                      left: 30,
+                      child: Container(
+                        height: 100,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: Colors.purple,
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text("Purple",style: TextStyle(
+                            fontWeight: FontWeight.bold,color: Colors.white
+                          ),),
+                        ),
+                      )),
+                  Positioned(
+                      top: 60,
+                      left: 60,
+                      child: Container(
+                        height: 100,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8.0),
+                          color: Colors.yellow,
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text("Yellow",style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            //vai White color dakha jai na daika black color dilam
+                            color: Colors.black
+                          ),),
+                        ),
+                      )),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );

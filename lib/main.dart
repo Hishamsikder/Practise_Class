d0efde7b85a795866c84b9678085be566334f8fd
Sanim018 <you@example.com>
2024-02-07 19:35:21 +0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
 }
+
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
@@ -11,148 +15,258 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  bool isSelected = false;
+  int? selectIndex;
+  @override
+  void initState() {
+    selectIndex = 0;
+    // TODO: implement initState
+    super.initState();
+  }
+  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Practise App"),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 30,
-              ),
-              Center(
-                child: Container(
-                  height: 50,
-                  width: 100,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    color: Colors.amber,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.shade600,
-                        offset: const Offset(0, 4),
-                        blurRadius: 5,
-                        spreadRadius: 1
-                      )
-                    ]
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              Container(
-                color: Colors.amber,
-                child: const Text("Class 3 Class 4"),
-              ),
-              const SizedBox(height: 30,),
-              Container(
-                height: 50,
-                width: 100,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    color: Colors.amber,
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.grey.shade600,
-                          offset: const Offset(0, -4),
-                          blurRadius: 5,
-                          spreadRadius: 1
-                      )
-                    ]
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Container(
-                height: 50,
-                width: 300,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.green),
-                    borderRadius: BorderRadius.circular(8),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        offset: const Offset(4, 4),
-                          color: Colors.grey.shade600,
-                          blurRadius: 5,
-                          spreadRadius: 1
-                      ),
-
-                    ]
-                ),
-                child: const Center(child: Padding(
-                  padding: EdgeInsets.only(right: 170.0),
-                  child: Text("influxdev.com"),
-                )),
-              ),
-              const SizedBox(height: 20,),
-              Stack(
-                clipBehavior: Clip.none,
-                children: [
-                  Container(
-                    height: 100,
-                    width: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      color: Colors.red,
-                    ),
-                    child: const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text("Red",style: TextStyle(
-                        fontWeight: FontWeight.bold,color: Colors.white
-                      ),),
-                    ),
-                  ),
-                  Positioned(
-                      top: 30,
-                      left: 30,
-                      child: Container(
-                        height: 100,
-                        width: 100,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8),
-                          color: Colors.purple,
-                        ),
-                        child: const Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text("Purple",style: TextStyle(
-                            fontWeight: FontWeight.bold,color: Colors.white
-                          ),),
-                        ),
-                      )),
-                  Positioned(
-                      top: 60,
-                      left: 60,
-                      child: Container(
-                        height: 100,
-                        width: 100,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8.0),
-                          color: Colors.yellow,
-                        ),
-                        child: const Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text("Yellow",style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            //vai White color dakha jai na daika black color dilam
-                            color: Colors.black
-                          ),),
-                        ),
-                      )),
-                ],
-              )
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          key: _scaffoldKey,
+          appBar: AppBar(
+            actions: [
+              Icon(Icons.more_vert),
             ],
+            backgroundColor: Colors.deepPurple,
+            title: const Text("Practise App"),
+          ),
+          drawer: Drawer(elevation: 10,
+            shadowColor: Colors.deepPurple,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListView(
+                children: [
+                  Column(
+                    children: [
+                      navDrawer(),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Center(
+                  child: ElevatedButton(onPressed: (){
+                    _scaffoldKey.currentState!.openDrawer();
+                  }, child: Text("Open Drawer")),
+                )
+              ],
+            ),
           ),
         ),
       ),
     );
   }
+
+  Widget navDrawer() {
+    return Material(
+      child: Column(
+        children: [
+          ListTile(
+            title: Text("Title"),
+            subtitle: Text("Subtext"),
+            focusColor: Colors.blue.shade200,
+            onFocusChange: (value) {},
+          ),
+          ListTile(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            onTap: () {
+              setState(() {
+                selectIndex = 0;
+              });
+            },
+            iconColor: selectIndex == 0 ? Color(0xff7760eb) : null,
+            tileColor: selectIndex == 0 ? Color(0xffcbc2f8) : null,
+            title: Text("Inbox",style: TextStyle(
+              color: selectIndex == 0 ? Color(0xffb67aef) : null,
+            ),),
+            leading: Icon(Icons.mail),
+          ),
+          ListTile(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            onTap: () {
+              setState(() {
+                selectIndex = 1;
+              });
+            },
+            iconColor: selectIndex == 1 ? Color(0xff7760eb) : null,
+            tileColor: selectIndex == 1 ? Color(0xffcbc2f8) : null,
+            title: Text("Outbox",style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: selectIndex == 1 ? Color(0xffb67aef) : null,
+            ),),
+            leading: Icon(Icons.send),
+          ), ListTile(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            onTap: () {
+              setState(() {
+                selectIndex = 2;
+              });
+            },
+            iconColor: selectIndex == 2 ? Color(0xff7760eb) : null,
+            tileColor: selectIndex == 2 ? Color(0xffcbc2f8) : null,
+            title: Text("Favourite",style: TextStyle(
+              color: selectIndex == 2 ? Color(0xffb67aef) : null,
+            ),),
+            leading: Icon(Icons.favorite_outlined),
+          ), ListTile(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            onTap: () {
+              setState(() {
+                selectIndex = 3;
+              });
+            },
+            iconColor: selectIndex == 3 ? Color(0xff7760eb) : null,
+            tileColor: selectIndex == 3 ? Color(0xffcbc2f8) : null,
+            title: Text("Archive",style: TextStyle(
+              color: selectIndex == 3 ? Color(0xffb67aef) : null,
+            ),),
+            leading: Icon(Icons.archive),
+          ), ListTile(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            onTap: () {
+              setState(() {
+                selectIndex = 4;
+              });
+            },
+            iconColor: selectIndex == 4 ? Color(0xff7760eb) : null,
+            tileColor: selectIndex == 4 ? Color(0xffcbc2f8) : null,
+            title: Text("Trash",style: TextStyle(
+              color: selectIndex == 4 ? Color(0xffb67aef) : null,
+            ),),
+            leading: Icon(Icons.delete),
+          ), ListTile(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            onTap: () {
+              setState(() {
+                selectIndex = 5;
+              });
+            },
+            iconColor: selectIndex == 5 ? Color(0xff7760eb) : null,
+            tileColor: selectIndex == 5 ? Color(0xffcbc2f8) : null,
+            title: Text("Spam",style: TextStyle(
+              color: selectIndex == 5 ? Color(0xffb67aef) : null,
+            ),),
+            leading: Icon(Icons.info),
+          ),
+          Divider(
+            color: Colors.black,
+          ),
+          ListTile(
+            leading: Text("Lebels"),
+          ),
+          ListTile(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            onTap: () {
+              setState(() {
+                selectIndex = 6;
+              });
+            },
+            iconColor: selectIndex == 6 ? Color(0xff7760eb) : null,
+            tileColor: selectIndex == 6 ? Color(0xffcbc2f8) : null,
+            title: Text("Family",style: TextStyle(
+              color: selectIndex == 6 ? Color(0xffb67aef) : null,
+            ),),
+            leading: Icon(Icons.library_add_check_rounded),
+          ),
+          ListTile(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            onTap: () {
+              setState(() {
+                selectIndex = 7;
+              });
+            },
+            iconColor: selectIndex == 7? Color(0xff7760eb) : null,
+            tileColor: selectIndex == 7 ? Color(0xffcbc2f8) : null,
+            title: Text("Friend",style: TextStyle(
+              color: selectIndex == 7? Color(0xffb67aef) : null,
+            ),),
+            leading: Icon(Icons.mobile_friendly),
+          ),
+          ListTile(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            onTap: () {
+              setState(() {
+                selectIndex = 8;
+              });
+            },
+            iconColor: selectIndex == 8? Color(0xff7760eb) : null,
+            tileColor: selectIndex == 8 ? Color(0xffcbc2f8) : null,
+            title: Text("Work",style: TextStyle(
+              color: selectIndex == 8 ? Color(0xffb67aef) : null,
+            ),),
+            leading: Icon(Icons.work),
+          ), ListTile(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            onTap: () {
+              setState(() {
+                selectIndex = 9;
+              });
+            },
+            iconColor: selectIndex == 9? Color(0xff7760eb) : null,
+            tileColor: selectIndex == 9 ? Color(0xffcbc2f8) : null,
+            title: Text("Setting & account",style: TextStyle(
+              color: selectIndex == 9 ? Color(0xffb67aef) : null,
+            ),),
+            leading: Icon(Icons.settings),
+          ),
+
+          ListTile(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            onTap: () {
+              _scaffoldKey.currentState!.openEndDrawer();
+              setState(() {
+                selectIndex = 10;
+              });
+            },
+            iconColor: selectIndex == 10? Color(0xff7760eb) : null,
+            tileColor: selectIndex == 10 ? Color(0xffcbc2f8) : null,
+            title: Text("Close Drawer",style: TextStyle(
+              color: selectIndex == 10? Color(0xffb67aef) : null,
+            ),),
+            leading: Icon(Icons.close),
+          ),
+
+        ],
+      ),
+    );
+  }
 }
+
+
